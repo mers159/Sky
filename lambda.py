ec280dcb36a7c374d60576cbc9cced3634a321d7
meriales <meriales.maryann05@gmail.com>
2020-10-28 19:22:00 +0800

    lambda python

---
+++ lambda.py
@@ -0,0 +1,10 @@
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"} #list
+]
+
+
+people.sort(key=lambda person: person["name"])
+
+print(people)
